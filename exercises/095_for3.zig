ba65a39391686cee649c8ca96a34dc8e7cabedc7
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2024-07-18 02:01:37 -0400

    more

--- exercises/095_for3.zig
+++ exercises/095_for3.zig
@@ -54,13 +54,10 @@ pub fn main() void {
 
     // I want to print every number between 1 and 20 that is NOT
     // divisible by 3 or 5.
-    for (???) |n| {
-
-        // The '%' symbol is the "modulo" operator and it
-        // returns the remainder after division.
-        if (n % 3 == 0) continue;
-        if (n % 5 == 0) continue;
-        std.debug.print("{} ", .{n});
+    for (1..21) |n| {
+        if (n % 3 != 0 and n % 5 != 0) {
+            std.debug.print("{} ", .{n});
+        }
     }
 
     std.debug.print("\n", .{});
